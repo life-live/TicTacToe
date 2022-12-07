60351e48a6001ea92e32b37669da68b625983241
Maksim Golikov <maksin-golikov@yandex.ru>
2022-12-07 20:43:03 +0300

    No Update

---
+++ TicTacToe/TicTacToe.Web/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Versioning;
+using Avalonia;
+using Avalonia.ReactiveUI;
+using Avalonia.Web;
+using TicTacToe;
+
+[assembly: SupportedOSPlatform("browser")]
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+            .UseReactiveUI()
+            .SetupBrowserApp("out");
+    }
+
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>();
+    }
+}
